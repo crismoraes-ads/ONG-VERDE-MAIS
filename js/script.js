f0a759c338bae102f43db5513e34c2f11b8bec17
Cristiane Moraes <cristecads@gmail.com>
2025-10-17 17:38:08 -0300

    Add js/script.js

---
+++ js/script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function setMask(input, maskFn) {
+    input.addEventListener("input", () => {
+      input.value = maskFn(input.value);
+    });
+  }
+
+  function maskCPF(v){
+    v = v.replace(/\D/g, "");
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    return v;
+  }
+
+  function maskTel(v){
+    v = v.replace(/\D/g, "");
+    if (v.length <= 10) {
+      v = v.replace(/(\d{2})(\d)/, "($1) $2");
+      v = v.replace(/(\d{4})(\d)/, "$1-$2");
+    } else {
+      v = v.replace(/(\d{2})(\d)/, "($1) $2");
+      v = v.replace(/(\d{5})(\d)/, "$1-$2");
+    }
+    return v;
+  }
+
+  function maskCEP(v){
+    v = v.replace(/\D/g, "");
+    v = v.replace(/(\d{5})(\d)/, "$1-$2");
+    return v;
+  }
+
+  const cpf = document.querySelector("#cpf");
+  const tel = document.querySelector("#telefone");
+  const cep = document.querySelector("#cep");
+
+  if (cpf) setMask(cpf, maskCPF);
+  if (tel) setMask(tel, maskTel);
+  if (cep) setMask(cep, maskCEP);
+
+  const form = document.querySelector("#form-cadastro");
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      if (!form.checkValidity()) {
+        e.preventDefault();
+        form.reportValidity();
+      } else {
+        e.preventDefault();
+        alert("Cadastro simulado: formulário válido!");
+      }
+    });
+  }
+});
